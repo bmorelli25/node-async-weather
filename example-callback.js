8768133d965ade09f9cb819d83c6caef97fa7f98
pentel13 <bmorelli25@gmail.com>
2016-09-26 14:20:04 -0700

    cleaning things up
    multiple files for easier reading/editing

--- example-callback.js
+++ example-callback.js
@@ -1,20 +1,5 @@
-var request = require('request');
-var apiKey = '';
-var url = `http://api.openweathermap.org/data/2.5/weather?q=Portland&units=imperial&APPID=${apiKey}`;
+var weather = require('./weather.js');
 
-request({
-  url: url,
-  json: true
-}, function (error, response, body) {
-  if (error) {
-    console.log('Unable to fetch Weather: ', error.message);
-  } else {
-    // Print entire JSON 'nicely'
-    // console.log(JSON.stringify(body, null, 4));
-
-    var location = body.name
-    var temp = body.main.temp;
-    var message = `It's ${temp} degrees in ${location}!`;
-    console.log(message);
-  };
-});
+ weather(function (currentWeather) {
+   console.log(currentWeather);
+ });
---
+++ weather.js
@@ -0,0 +1,23 @@
+var request = require('request');
+
+var apiKey = '';
+var url = `http://api.openweathermap.org/data/2.5/weather?q=Portland&units=imperial&APPID=${apiKey}`;
+
+module.exports = function (callback) {
+  request({
+    url: url,
+    json: true
+  }, function (error, response, body) {
+    if (error) {
+      callback('Unable to fetch Weather: ', error.message);
+    } else {
+      // Print entire JSON 'nicely'
+      // console.log(JSON.stringify(body, null, 4));
+
+      var location = body.name
+      var temp = body.main.temp;
+      var message = `It's ${temp} degrees in ${location}!`;
+      callback(message);
+    };
+  });
+};
