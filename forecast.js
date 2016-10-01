0ded5a68307937cc091c56917647b96e10270d20
Brandon Morelli <bmorelli25@gmail.com>
2016-10-01 16:34:11 -0700

    Updating to show full 5 day forecast
    - Was showing just the next day at midnight. Updated to show the next 5 days @ 3pm.
    - Removed 'us' in API link to allow searches outside the United States

--- forecast.js
+++ forecast.js
@@ -4,7 +4,7 @@ module.exports = function (location) {
   return new Promise (function (resolve, reject) {
     var apiKey = process.env.API_KEY;
     console.log(location);
-    var url = `http://api.openweathermap.org/data/2.5/forecast?q=${encodeURIComponent(location)},us&mode=json&units=imperial&APPID=${apiKey}`;
+    var url = `http://api.openweathermap.org/data/2.5/forecast?q=${encodeURIComponent(location)}&mode=json&units=imperial&APPID=${apiKey}`;
 
     if (!location) {
       reject('No Location Provided');
@@ -18,8 +18,22 @@ module.exports = function (location) {
         reject('Unable to fetch Weather: ', error.message);
       } else { // Print entire JSON 'nicely': console.log(JSON.stringify(body, null, 4));
         var location = body.city.name;
-        var temp = body.list[0].main.temp;
-        var message = `The 5 day forecasted temperature in ${location} is ${temp} degrees!`;
+        var temp1 = body.list[5].main.temp;
+        var date1 = body.list[5].dt_txt;
+        var temp2 = body.list[13].main.temp;
+        var date2 = body.list[13].dt_txt;
+        var temp3 = body.list[21].main.temp;
+        var date3 = body.list[21].dt_txt;
+        var temp4 = body.list[29].main.temp;
+        var date4 = body.list[29].dt_txt;
+        var temp5 = body.list[37].main.temp;
+        var date5 = body.list[37].dt_txt;
+        var message = `Here is the 5 day forecasted temperature in ${location}:
+         ${date1}: ${temp1} degrees
+         ${date2}: ${temp2} degrees
+         ${date3}: ${temp3} degrees
+         ${date4}: ${temp4} degrees
+         ${date5}: ${temp5} degrees`;
         resolve(message);
       };
     });
