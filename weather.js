34f98fb3b8dba8818422d4f10ab6d663ac57bc28
Brandon Morelli <bmorelli25@gmail.com>
2016-10-01 16:09:54 -0700

    renaming apiKey to API_KEY

--- weather.js
+++ weather.js
@@ -2,12 +2,12 @@ var request = require('request');
 
 module.exports = function (location) {
   return new Promise (function (resolve, reject) {
-    var apiKey = process.env.API_KEY;
-    if(typeof(apiKey) == "undefined" || apiKey.length == 0) {
+    var API_KEY = process.env.API_KEY;
+    if(typeof(API_KEY) == "undefined" || API_KEY.length == 0) {
       return reject("No API key set. (Create a .env file or set an API_KEY environment variable)");
     }
 
-    var url = `http://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(location)}&units=imperial&APPID=${apiKey}`;
+    var url = `http://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(location)}&units=imperial&APPID=${API_KEY}`;
 
     if (!location) {
       reject('No Location Provided');
