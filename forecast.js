853605ec28080cb40b8f4a9b360cbdf2c50e0408
Mokhtar <mokhtar.mial@gmail.com>
2016-10-02 21:56:53 +0200

    Forecast working as expected now

--- forecast.js
+++ forecast.js
@@ -21,20 +21,80 @@ module.exports = function (location) {
       if (error) {
         reject('Unable to fetch Weather: ', error.message);
       } else { // Print entire JSON 'nicely': console.log(JSON.stringify(body, null, 4));
-      console.log(JSON.stringify(body, null, 4));
         let message = ``;
         if(body.cod == 200) {
           let location = body.city.name,
-              table = new Table({ head: [location, "00:00", "03:00", "06:00", "09:00", "12:00", "15:00", "18:00", "21:00"] }),
+              table = new Table({ head: [location, '00:00', '03:00', '06:00', '09:00', '12:00', '15:00', '18:00', '21:00'] }),
               row = {},
-              i = 0;
+              i = 0,
+              length = body.list.length;
           for(const item of body.list){
             let leftHeader = `${item.dt_txt.substring(0,10)}`;
-            row[leftHeader] = row[leftHeader] || [];
+            let temp = []; //weird stuff to have the date in the first column
+            temp.push(leftHeader);
+            row[leftHeader] = row[leftHeader] || temp;
+            if (i === 0) { //check which hour we get first on the first day of data
+              //weird stuff to have blank cases
+              switch (item.dt_txt.substring(11)) {
+                case '00:00:00':
+                  break;
+                case '03:00:00':
+                  for (let i = 0; i < 1; i++) {
+                    row[leftHeader].push('');
+                  }
+                  i = 1;
+                  length += i;
+                  break;
+                case '06:00:00':
+                  for (let i = 0; i < 2; i++) {
+                    row[leftHeader].push('');
+                  }
+                  i = 2;
+                  length += i;
+                  break;
+                case '09:00:00':
+                  for (let i = 0; i < 3; i++) {
+                    row[leftHeader].push('');
+                  }
+                  i = 3;
+                  length += i;
+                  break;
+                case '12:00:00':
+                  for (let i = 0; i < 4; i++) {
+                    row[leftHeader].push('');
+                  }
+                  i = 4;
+                  length += i;
+                  break;
+                case '15:00:00':
+                  for (let i = 0; i < 5; i++) {
+                    row[leftHeader].push('');
+                  }
+                  i = 5;
+                  length += i;
+                  break;
+                case '18:00:00':
+                  for (let i = 0; i < 6; i++) {
+                    row[leftHeader].push('');
+                  }
+                  i = 6;
+                  length += i;
+                  break;
+                case '21:00:00':
+                  for (let i = 0; i < 7; i++) {
+                    row[leftHeader].push('');
+                  }
+                  i = 7;
+                  length += i;
+                  break;
+                default:
+                  break;
+              }
+            }
             row[leftHeader].push(`${item.main.temp}`);
             i++;
-            if(i%8 === 0){
-              table.push(row);
+            if(i%8 === 0 || i == length){
+              table.push(row[leftHeader]);
               row = {};
             }
           }
